5f3fc4468b2d3c2143c7519678c0407fc6b5f614
Mahmud Hasan Jion <mahmudh.jion02@gmail.com>
2025-03-29 06:27:50 +0000

    Add custom API routes for items and update default controller comment

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,9 +49,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
+$route['default_controller'] = 'welcome'; // Change 'welcome' to your default controller
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+// Example custom route
+$route['api/items'] = 'items_controller/index';
+$route['api/items/(:num)'] = 'items_controller/show/$1';
+
 $route['templates/(:any)'] = "templates/view/$1";
 $route['items'] = "items/index";
 $route['itemsCreate']['post'] = "items/store";
